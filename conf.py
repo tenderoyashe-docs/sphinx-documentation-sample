ecf53f0d13a88767c6ab3e89bc7bcbdb6bd90c5c
tenderoyashe-docs <tendizhou@gmail.com>
2026-02-24 01:33:42 +0100

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,13 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'TaskFlow Documentation'
+author = 'Tenderoyashe Zhou'
+release = '1.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
